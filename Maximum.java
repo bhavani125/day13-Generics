7456fdb86cbbee7cac50512bb50228722794967d
bhavani125 <girinenibhavani12@gmil.com>
2021-07-15 18:26:24 +0530

    Added: Welcome Message

---
+++ Maximum.java
@@ -0,0 +1,8 @@
+package com.max;
+
+public class Maximum {
+    //main method
+    public static void main(String[] args) {
+        System.out.println("Welcome to find maximum of numbers");
+    }
+}
