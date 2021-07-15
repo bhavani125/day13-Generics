a4ac57748cf9b8989d4e211908a67fff76fb2ccd
bhavani125 <girinenibhavani12@gmil.com>
2021-07-15 18:50:58 +0530

    Added: Code to finding maximum of 3 floats

--- Maximum.java
+++ Maximum.java
@@ -3,20 +3,23 @@ package com.max;
 public class Maximum {
     //main method
     public static void main(String[] args) {
-        Integer a=2;
-        Integer b=4;
-        Integer c=6;
-        findMaximum(a,b,c);
+        Float x = 3.6f;
+        Float y = 4.8f;
+        Float z = 5.9f;
+        findMaximum(x,y,z);
+
     }
-    //creating method for findMaximum
-    private static void findMaximum(Integer a, Integer b, Integer c) {
-        Integer max = a;
-        if (b.compareTo(a)>0) {
-            max = b;
+    //Creating method for findMaximum
+    private static void findMaximum(Float x, Float y, Float z) {
+        Float max = x;
+        if (y.compareTo(x)>0) {
+            max = y;
         }
-        if (c.compareTo(max)>0) {
-            max = c;
+        if (z.compareTo(max)>0) {
+            max = z;
         }
         System.out.println("Maximum of numbers are " +max);
+
     }
+
 }
