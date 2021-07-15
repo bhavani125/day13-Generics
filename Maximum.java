4e60e7c588db92820c5771a49c56fdc47dc5721a
bhavani125 <girinenibhavani12@gmil.com>
2021-07-15 18:40:09 +0530

    Added: Code to finding maximum of 3 integers

--- Maximum.java
+++ Maximum.java
@@ -3,6 +3,20 @@ package com.max;
 public class Maximum {
     //main method
     public static void main(String[] args) {
-        System.out.println("Welcome to find maximum of numbers");
+        Integer a=2;
+        Integer b=4;
+        Integer c=6;
+        findMaximum(a,b,c);
+    }
+    //creating method for findMaximum
+    private static void findMaximum(Integer a, Integer b, Integer c) {
+        Integer max = a;
+        if (b.compareTo(a)>0) {
+            max = b;
+        }
+        if (c.compareTo(max)>0) {
+            max = c;
+        }
+        System.out.println("Maximum of numbers are " +max);
     }
 }
