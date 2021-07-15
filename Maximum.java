fe8addc020939bbb38a936c8b0d3c563efc21caa
bhavani125 <girinenibhavani12@gmil.com>
2021-07-15 22:05:24 +0530

    Refactoring class as generic

--- Maximum.java
+++ Maximum.java
@@ -1,11 +1,25 @@
 package com.max;
-
-public class Maximum {
-    //main method
+//creating class as generic
+public class Maximum <E extends Comparable<E>> {
+    //creating 3 generic type variables
+    E num1;
+    E num2;
+    E num3;
+    public Maximum(E num1, E num2, E num3) {
+        super();
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+    }
+    // main method
     public static void main(String[] args) {
-        findMax(4,6,8);
-        findMax(1.1,1.2,1.3);
-        findMax("apple","peach","banana");
+        System.out.println("Maximum Integer by using Generic class :" + new Maximum(2, 4, 6).FindMax());
+        System.out.println("Maximum Float by using Generic class :" + new Maximum(1.1, 2.2, 3.3).FindMax());
+        System.out.println("Maximum String by using Generic class :" + new Maximum("apple","peach","banana").FindMax());
+    }
+    // generic method
+    public E FindMax() {
+        return Maximum.findMax(num1, num2, num3);
     }
 
     public static <E extends Comparable <E>> E findMax(E num1, E num2, E num3) {
@@ -20,5 +34,6 @@ public class Maximum {
         System.out.printf("Maximum of numbers are " + max +"\n");
         return max;
     }
+
 
 }
